8ba06a4c980b6780e57575cc7fb57740f7bebebb
DiazRock <corolariodiaz@gmail.com>
2021-05-07 22:29:54 -0400

    Modified the app module

--- backend-service/src/app.module.ts
+++ backend-service/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongodb:27017/nest'), 
+    BlogModule,
+    ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
