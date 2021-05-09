445b0c4c4b8abe34d9259484eaa9ca3440072450
DiazRock <corolariodiaz@gmail.com>
2021-05-09 17:09:10 -0400

    Added row

--- client-service/src/App.js
+++ client-service/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import { GlobalStyle } from './GlobalStyles'
 import { Header } from './components/header'
+import { Row } from './components/row'
+import { post_fetcher } from './api_requester/post_fetcher'
+
+
 
 function App() {
+  const feed = post_fetcher()
   return (
     <div>
       <Header />
+      <section>
+        <Row 
+          title="HN Feed pablito clavo un clavito" 
+          author="Lindoro"
+          date_time="La cosa por acá"
+
+        />
+      </section>
+      
+      <hr />
       <GlobalStyle />
     </div>
   );
