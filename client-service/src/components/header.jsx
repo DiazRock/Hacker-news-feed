6fac5f939200c1d506bf410c26d13dd9187db56b
DiazRock <corolariodiaz@gmail.com>
2021-05-09 17:09:23 -0400

    Modified header

--- client-service/src/components/header.jsx
+++ client-service/src/components/header.jsx
@@ -3,9 +3,13 @@ import { HeaderStyle, H1, P } from './styles';
 
 export const Header = () => (
     <HeaderStyle>
+        <br />
+        <br />
+        <br />
         <H1>
             HN Feed
         </H1>
+        <br />
         <P> We {'<'}3 hacker news !</P>  
     </HeaderStyle>
 )
