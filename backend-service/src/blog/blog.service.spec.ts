a037c2f0b0054b151a83ef22dc58ecc5960009bd
DiazRock <corolariodiaz@gmail.com>
2021-05-10 21:41:29 -0400

    Test for service

--- backend-service/src/blog/blog.service.spec.ts
+++ backend-service/src/blog/blog.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogService } from './blog.service';
+import { mockBlog } from '../../test/mocks/mockblog'
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('BlogService', () => {
   let service: BlogService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogService],
+      providers: [
+        BlogService,
+        {
+          provide: getModelToken('Blog'),
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(mockBlog())
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<BlogService>(BlogService);
